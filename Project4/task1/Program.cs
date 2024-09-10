93824e0b288ef96fab3dd4d19a7cce225daebeb5
Фёдор Лодиков <fedor_ramoz@mail.ru>
2024-09-10 23:47:11 +0300

    модули

---
+++ Project4/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение:");
+        string input = Console.ReadLine() ?? string.Empty; 
+        string[] words = SplitText(input);
+        PrintWords(words);
+    }
+
+    static string[] SplitText(string text)
+    {
+        return text.Split(' ');
+    }
+
+    static void PrintWords(string[] words)
+    {
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ Project4/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение:");
+        string input = Console.ReadLine() ?? string.Empty; 
+
+        string reversedPhrase = ReverseWords(input);
+        Console.WriteLine("Предложение с перевёрнутыми словами:");
+        Console.WriteLine(reversedPhrase);
+    }
+
+    static string[] SplitText(string text)
+    {
+        return text.Split(' ', StringSplitOptions.RemoveEmptyEntries); 
+    }
+
+    static string ReverseWords(string inputPhrase)
+    {
+        string[] words = SplitText(inputPhrase);
+        Array.Reverse(words); 
+
+        return string.Join(' ', words); 
+    }
+}
